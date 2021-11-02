f8160ae3641d20486e82b278a6e693a4b91beccd
christopherforsythe <christopherforsythe123@gmail.com>
2021-11-02 15:15:26 +0000

    initial commit to tasker app

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./components/App";
+
+//6. Make sure that the final website is styled like the example shown here:
+//https://l1pp6.csb.app/
+ReactDom.render(<App />, document.getElementById("root"));
+//HINT: You will need to study the classes in teh styles.css file to appy styling.
